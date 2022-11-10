c4fc38a65c67a0eacb904a771975e00edc556152
Oliver Samuels <osamuels@bu.edu>
2022-11-10 14:23:27 -0500

    QuickFixList

---
+++ app/src/main/java/com/example/vitamin_app/QuickFixList.java
@@ -0,0 +1,57 @@
+package com.example.vitamin_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+public class QuickFixList extends Fragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//    private String mParam1;
+//    private String mParam2;
+
+    public QuickFixList() {
+        // Required empty public constructor
+    }
+
+//    public static QuickFixList newInstance(String param1, String param2) {
+//        QuickFixList fragment = new QuickFixList();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button age = (Button) getView().findViewById(R.id.agingButton);
+        age.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(view.getContext(), HomeActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+//                view.getContext().startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quick_fix_list, container, false);
+    }
+}
