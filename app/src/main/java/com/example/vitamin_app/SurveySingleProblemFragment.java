4aa23c9f1de321f422f5f0c9e402dfdb5d0bd9e1
luca1007 <luca1007@bu.edu>
2022-11-17 15:45:42 -0500

    Finalized survey layout

--- app/src/main/java/com/example/vitamin_app/SurveySingleProblemFragment.java
+++ app/src/main/java/com/example/vitamin_app/SurveySingleProblemFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SurveySingleProblemFragment extends Fragment {
 
@@ -25,8 +26,10 @@ public class SurveySingleProblemFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_survey_single_problem, container, false);
 
         Bundle bundle = this.getArguments();
+        TextView header = (TextView) v.findViewById(R.id.problem_header);
 
         if (bundle.getBoolean("weight")) {
+            header.setText("Weight Loss");
             Fragment triple = new SurveyTripleCheckboxFragment();
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -35,6 +38,7 @@ public class SurveySingleProblemFragment extends Fragment {
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         } else if (bundle.getBoolean("sleep")) {
+            header.setText("Sleep");
             Fragment triple = new SurveyTripleCheckboxFragment();
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -43,6 +47,7 @@ public class SurveySingleProblemFragment extends Fragment {
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         } else if (bundle.getBoolean("energy")) {
+            header.setText("Energy");
             Fragment doublle = new SurveyDoubleCheckboxFragment();
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -51,6 +56,7 @@ public class SurveySingleProblemFragment extends Fragment {
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         } else if (bundle.getBoolean("immunity")) {
+            header.setText("Immunity");
             Fragment doublle = new SurveyDoubleCheckboxFragment();
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -59,6 +65,7 @@ public class SurveySingleProblemFragment extends Fragment {
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         } else if (bundle.getBoolean("skin")) {
+            header.setText("Skin");
             Fragment triple = new SurveyTripleCheckboxFragment();
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -67,6 +74,7 @@ public class SurveySingleProblemFragment extends Fragment {
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         } else if (bundle.getBoolean("detox")) {
+            header.setText("Detox");
             Fragment triple = new SurveyTripleCheckboxFragment();
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -75,6 +83,7 @@ public class SurveySingleProblemFragment extends Fragment {
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         } else if (bundle.getBoolean("exercise")) {
+            header.setText("Exercise");
             Fragment doublle = new SurveyDoubleCheckboxFragment();
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -83,6 +92,7 @@ public class SurveySingleProblemFragment extends Fragment {
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         } else if (bundle.getBoolean("digestion")) {
+            header.setText("Digestion");
             Fragment doublle = new SurveyDoubleCheckboxFragment();
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -91,12 +101,14 @@ public class SurveySingleProblemFragment extends Fragment {
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         } else if (bundle.getBoolean("articulation")) {
+            header.setText("Articulation");
             Fragment doublle = new SurveyDoubleCheckboxFragment();
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             doublle.setArguments(bundle);
             fragmentTransaction.replace(R.id.checkboxLayout, doublle);
             fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
         }
 
         return v;
