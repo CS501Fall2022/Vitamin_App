d53fdd435ac93808a80dee882ebd0b786b2e50eb
ryanv249 <ryanv249@bu.edu>
2022-11-17 16:33:13 -0500

    simple implementation complete. need to make pretty and pull display from db.

--- app/src/main/java/com/example/vitamin_app/ResultListActivity.java
+++ app/src/main/java/com/example/vitamin_app/ResultListActivity.java
@@ -1,8 +1,12 @@
 package com.example.vitamin_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.ReceiverCallNotAllowedException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +14,16 @@ import android.widget.ImageButton;
 
 public class ResultListActivity extends AppCompatActivity {
 
+    RecyclerView resultDisplay;
+    ResultListAdapter rvAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_list);
+
+        resultDisplay = findViewById(R.id.resultDisplay);
+        rvAdapter = setAndGetAdapter(resultDisplay);
 
         ImageButton toHome = (ImageButton) findViewById(R.id.toHome);
         toHome.setOnClickListener(new View.OnClickListener() {
@@ -22,7 +32,6 @@ public class ResultListActivity extends AppCompatActivity {
                 Intent intent = new Intent(view.getContext(), HomeActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                 view.getContext().startActivity(intent);
-
             }
         });
 
@@ -45,5 +54,12 @@ public class ResultListActivity extends AppCompatActivity {
                 view.getContext().startActivity(intent);
             }
         });
+    }
+
+    private ResultListAdapter setAndGetAdapter(RecyclerView rv){
+        ResultListAdapter adapter = new ResultListAdapter(this);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(adapter);
+        return adapter;
     }
 }
