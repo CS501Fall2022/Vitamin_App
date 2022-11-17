736ae5b48629da675ceff04f12aaf101b729926e
luca1007 <luca1007@bu.edu>
2022-11-17 16:54:31 -0500

    Linked app to firebase realtime database

--- app/src/main/java/com/example/vitamin_app/SurveySingleProblemFragment.java
+++ app/src/main/java/com/example/vitamin_app/SurveySingleProblemFragment.java
@@ -2,6 +2,7 @@ package com.example.vitamin_app;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,9 +10,28 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class SurveySingleProblemFragment extends Fragment {
+
+    // creating a variable for our
+    // Firebase Database.
+    FirebaseDatabase firebaseDatabase;
+
+    // creating a variable for our Database
+    // Reference for Firebase.
+    DatabaseReference databaseReference;
+
+    // creating a variable for
+    // our object class
+    Users users;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +47,15 @@ public class SurveySingleProblemFragment extends Fragment {
 
         Bundle bundle = this.getArguments();
         TextView header = (TextView) v.findViewById(R.id.problem_header);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // below line is used to get reference for our database.
+        databaseReference = firebaseDatabase.getReference("Users");
+
+        // initializing our object
+        // class variable.
+        users = new Users();
 
         if (bundle.getBoolean("weight")) {
             header.setText("Weight Loss");
@@ -110,6 +139,46 @@ public class SurveySingleProblemFragment extends Fragment {
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
         }
+
+        Button endSurvey = (Button) v.findViewById(R.id.endSurvey);
+        endSurvey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bundle.getBoolean("weight")) {
+                    users.setProblem("weight");
+                } else if (bundle.getBoolean("sleep")) {
+                    users.setProblem("sleep");
+                } else if (bundle.getBoolean("energy")) {
+                    users.setProblem("energy");
+                } else if (bundle.getBoolean("immunity")) {
+                    users.setProblem("immunity");
+                } else if (bundle.getBoolean("skin")) {
+                    users.setProblem("skin");
+                } else if (bundle.getBoolean("detox")) {
+                    users.setProblem("detox");
+                } else if (bundle.getBoolean("exercise")) {
+                    users.setProblem("exercise");
+                } else if (bundle.getBoolean("digestion")) {
+                    users.setProblem("digestion");
+                } else if (bundle.getBoolean("articulation")) {
+                    users.setProblem("articulation");
+                }
+
+                /*
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        databaseReference.setValue(users);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+                 */
+            }
+        });
 
         return v;
     }
