1b803ee233bb8ed10d3c62fb7acd5498516705db
ryanv249 <ryanv249@bu.edu>
2022-11-28 16:56:43 -0500

    merged with 2.0 branch

--- app/src/main/java/com/example/vitamin_app/ResultListActivity.java
+++ app/src/main/java/com/example/vitamin_app/ResultListActivity.java
@@ -13,7 +13,6 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 public class ResultListActivity extends AppCompatActivity {
-
     RecyclerView resultDisplay;
     ResultListAdapter rvAdapter;
 
